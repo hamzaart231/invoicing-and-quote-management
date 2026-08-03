55c24c9529eea0f22933174554498dd422771135
hamzaart231 <idmichoponline@gmail.com>
2026-08-04 00:32:21 +0100

    Add currencies data structure with various currencies

---
+++ src/lib/currencies.ts
@@ -0,0 +1,9 @@
+export const currencies = [
+  { code: "USD", symbol: "$", name: "US Dollar" },
+  { code: "EUR", symbol: "€", name: "Euro" },
+  { code: "GBP", symbol: "£", name: "British Pound" },
+  { code: "MAD", symbol: "د.م.", name: "Moroccan Dirham" },
+  { code: "SAR", symbol: "﷼", name: "Saudi Riyal" },
+  { code: "AED", symbol: "د.إ", name: "UAE Dirham" },
+  { code: "EGP", symbol: "£", name: "Egyptian Pound" }
+];
