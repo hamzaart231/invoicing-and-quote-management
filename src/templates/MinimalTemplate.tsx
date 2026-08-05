3312d82153afa08f7f5ec552ed45766c9d99b29e
hamzaart231 <idmichoponline@gmail.com>
2026-08-06 00:04:12 +0100

    Refactor MinimalTemplate to use ModernTemplate

--- src/templates/MinimalTemplate.tsx
+++ src/templates/MinimalTemplate.tsx
@@ -1,3 +1,10 @@
-export default function MinimalTemplate() {
-  return null;
+"use client";
+
+import ModernTemplate from "./ModernTemplate";
+import { DocumentTemplateProps } from "./types";
+
+export default function MinimalTemplate(
+  props: DocumentTemplateProps
+) {
+  return <ModernTemplate {...props} />;
 }
