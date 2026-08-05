0e298a71c70f98a34e2e227678946ce8d8b60099
hamzaart231 <idmichoponline@gmail.com>
2026-08-06 00:01:37 +0100

    Refactor ClassicTemplate to use ModernTemplate

--- src/templates/ClassicTemplate.tsx
+++ src/templates/ClassicTemplate.tsx
@@ -1,3 +1,10 @@
-export default function ClassicTemplate() {
-  return null;
+"use client";
+
+import ModernTemplate from "./ModernTemplate";
+import { DocumentTemplateProps } from "./types";
+
+export default function ClassicTemplate(
+  props: DocumentTemplateProps
+) {
+  return <ModernTemplate {...props} />;
 }
